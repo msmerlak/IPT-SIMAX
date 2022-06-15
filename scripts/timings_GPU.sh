c5551fab1a2d3d2df308bf6ff4bee2cf9d662307
Matteo Smerlak <matteo.smerlak@gmail.com>
2022-06-15 10:07:48 +0200

    another small fix

--- scripts/timings_GPU.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --nodes=1
-#SBATCH --mem=0
-#SBATCH --partition=cuda
-#SBATCH --mail-user=smerlak@mis.mpg.de
-#SBATCH --mail-type=ALL
-
-julia timings_GPU.jl > timings_GPU.log
